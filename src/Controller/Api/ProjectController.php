beb6ae123fd2badb4c751c0b05499e9d117ec167
Rytis Tekorius <rytis@tekorius.com>
2018-07-25 11:54:26 +0300

    Fix Api Doc

--- src/Controller/Api/ProjectController.php
+++ src/Controller/Api/ProjectController.php
@@ -3,14 +3,22 @@
 namespace App\Controller\Api;
 
 use App\Entity\Project;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Swagger\Annotations as SWG;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
+ * @SWG\Tag(name="Projects")
  * @Route("/projects", name="projects_")
  */
 class ProjectController extends Controller
 {
     /**
+     * Get projects assigned to the current user
+     *
+     * @SWG\Response(response=200, description="")
+     *
+     * @Method("GET")
      * @Route("", name="index")
      */
     public function index()
--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -1,14 +1,23 @@
 <?php
 
 namespace App\Controller\Api;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Swagger\Annotations as SWG;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
+ * @SWG\Tag(name="User")
  * @Route("/users", name="user_")
  */
 class UserController extends Controller
 {
     /**
+     * Get current user's info
+     *
+     * @SWG\Response(response=200, description="")
+     *
+     * @Method("GET")
      * @Route("/me", name="me")
      */
     public function me()
